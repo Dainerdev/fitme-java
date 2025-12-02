4eb475690917153e25c148ed1fa597b89409dc12
Dainerdev <florianarroyo15@gmail.com>
2025-12-01 21:38:24 -0500

    Inicio del Proyecto FitMe

---
+++ src/main/java/com/dainerdev/fitme/FitmeApplication.java
@@ -0,0 +1,13 @@
+package com.dainerdev.fitme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmeApplication.class, args);
+	}
+
+}
